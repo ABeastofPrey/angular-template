3201b10f7afcc092974faf269b699ae3c21765a9
WenBo <wenbo.wang1@kuka.com>
2021-04-09 16:31:38 +0800

    add page-not-found page

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { PagesModule } from './pages/pages.module';
+import { ShareRoutingModule } from './share-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ShareRoutingModule,
+    // PagesModule
   ]
 })
 export class ShareModule { }
