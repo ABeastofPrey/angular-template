858d8c95c8e1365085c134dbfb798498726cbc17
WenBo <wenbo.wang1@kuka.com>
2021-04-12 15:32:25 +0800

    remove register

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignOutComponent } from './components/sign-out/sign-out.component';
-import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     SignInComponent,
     SignOutComponent,
-    RegisterComponent
   ],
   imports: [
     CommonModule,
