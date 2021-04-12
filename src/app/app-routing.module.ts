5e084578228029a38710cbb4efe85ce7c1ea8c25
WenBo <wenbo.wang1@kuka.com>
2021-04-12 16:50:18 +0800

    add preload module strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadModuleStrategyService } from './share/services/preload-module-strategy/preload-module-strategy.service';
 
 const routes: Routes = [
   {
     path: 'auth',
+    data: { preload: true },
     loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule)
   }, {
     path: '',
+    data: { preload: true },
     loadChildren: () => import('./core/home/home.module').then(m => m.HomeModule)
   }, {
     path: '**', redirectTo: 'pages', pathMatch: 'full'
@@ -14,9 +17,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(
-    routes
-    // , { relativeLinkResolution: 'legacy' }
+  providers: [PreloadModuleStrategyService],
+  imports: [
+    RouterModule.forRoot(
+      routes
+      , {
+        // relativeLinkResolution: 'legacy',
+        preloadingStrategy: PreloadModuleStrategyService
+      }
     )],
   exports: [RouterModule]
 })
