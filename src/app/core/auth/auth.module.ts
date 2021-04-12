c0732e78e96080ae3daad544b747bb45b0194890
WenBo <wenbo.wang1@kuka.com>
2021-04-12 15:37:02 +0800

    add sign-up

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignOutComponent } from './components/sign-out/sign-out.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
     SignInComponent,
     SignOutComponent,
+    SignUpComponent,
   ],
   imports: [
     CommonModule,
