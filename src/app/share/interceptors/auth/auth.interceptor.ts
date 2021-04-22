d122392661f3cafa800718b1aaea680233bc6a44
WenBo <wenbo.wang1@kuka.com>
2021-04-22 17:43:53 +0800

    don't add token to login

--- src/app/share/interceptors/auth/auth.interceptor.ts
+++ src/app/share/interceptors/auth/auth.interceptor.ts
@@ -14,6 +14,8 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url.indexOf('/login') !== -1) return next.handle(request);
+
     const token = this.auth.getToken();
     const authRequest = request.clone({ setHeaders: { Authorization: token } });
     return next.handle(authRequest);
