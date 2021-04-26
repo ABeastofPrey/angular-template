4ee26de18d8ff360cf637df25d49ef1787466fab
WenBo <wenbo.wang1@kuka.com>
2021-04-26 17:36:41 +0800

    init nz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { CoreModule } from './core-features/core.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './_share-interceptors';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -17,10 +24,11 @@ import { httpInterceptorProviders } from './_share-interceptors';
     AppRoutingModule,
     CoreModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   exports: [],
-  providers: [httpInterceptorProviders],
+  providers: [httpInterceptorProviders, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
