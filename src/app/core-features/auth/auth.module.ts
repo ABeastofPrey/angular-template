c5b835878e1ab0a86e04f083e0a2f93c0abb86ab
WenBo <wenbo.wang1@kuka.com>
2021-04-27 11:29:47 +0800

    support IE 11

--- src/app/core-features/auth/auth.module.ts
+++ src/app/core-features/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignOutComponent } from './components/sign-out/sign-out.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @NgModule({
@@ -17,7 +19,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NzInputModule,
+    NzButtonModule
   ]
 })
 export class AuthModule { }
