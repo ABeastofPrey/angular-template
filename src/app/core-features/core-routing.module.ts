f9513e5fe78036bbbd93ce9c67e174f3ac86aeef
WenBo <wenbo.wang1@kuka.com>
2021-04-28 17:04:10 +0800

    add profile module

--- src/app/core-features/core-routing.module.ts
+++ src/app/core-features/core-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  {
-    path: 'user-dashboard',
-    loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule)
-  }
-];
+const routes: Routes = [{
+  path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+}, {
+  path: 'user-dashboard',
+  loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule)
+}, {
+  path: 'profile',
+  loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/core-features/profile/profile.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path: '', component: MyProfileComponent
+}];
+
+@NgModule({
+  declarations: [
+    MyProfileComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProfileModule { }
