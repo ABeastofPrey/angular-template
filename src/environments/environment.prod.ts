c0574a85838592ce88c375b17e1bbce8b537a74a
WenBo <wenbo.wang1@kuka.com>
2021-05-08 13:58:08 +0800

    fix location issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
-const { protocol, hostname, port } = location;
+// const { protocol, hostname, port } = location;
+const protocol = 'http:';
+const hostname = 'localhost';
+const port = '8090';
 const apiUrl = `${protocol}//${hostname}:${port}/`;
 
 export const environment = {
